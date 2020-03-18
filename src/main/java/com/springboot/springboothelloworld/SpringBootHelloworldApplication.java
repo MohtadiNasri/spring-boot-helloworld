e976bd384992bcbc33e8e0f10e18c00920cb7b22
nasrimohtadi@gmail.com <Awkward@009>
2020-03-18 22:13:54 +0100

    First HTML File

--- src/main/java/com/springboot/springboothelloworld/SpringBootHelloworldApplication.java
+++ src/main/java/com/springboot/springboothelloworld/SpringBootHelloworldApplication.java
@@ -1,12 +1,41 @@
 package com.springboot.springboothelloworld;
 
+import com.springboot.springboothelloworld.servlet.HelloFilter;
+import com.springboot.springboothelloworld.servlet.HelloListener;
+import com.springboot.springboothelloworld.servlet.HelloServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@ServletComponentScan
 public class SpringBootHelloworldApplication {
+
+	/// Register Servlet.
+	@Bean
+	public ServletRegistrationBean getServletRegistrationBean() {
+		ServletRegistrationBean servletBean = new ServletRegistrationBean(new HelloServlet());
+		servletBean.addUrlMappings("/helloServlet");
+		return servletBean;
+	}
+
+	/// Register Filter.
+	@Bean
+	public FilterRegistrationBean getFilterRegistrationBean() {
+		FilterRegistrationBean filterBean = new FilterRegistrationBean(new HelloFilter());
+		// Add filter path
+		filterBean.addUrlPatterns("/helloServlet");
+		return filterBean;
+	}
+
+	@Bean
+	public ServletListenerRegistrationBean<HelloListener> getServletListenerRegistrationBean() {
+		ServletListenerRegistrationBean listenerBean =
+				new ServletListenerRegistrationBean(new HelloListener());
+		return listenerBean;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloworldApplication.class, args);
