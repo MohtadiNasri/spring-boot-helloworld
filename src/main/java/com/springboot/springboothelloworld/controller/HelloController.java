91658d752f732be7859b3558ea35e53d0aea31ac
nasrimohtadi@gmail.com <Awkward@009>
2020-03-18 21:13:27 +0100

    Changed ResponseBody and Controller with RestController

--- src/main/java/com/springboot/springboothelloworld/controller/HelloController.java
+++ src/main/java/com/springboot/springboothelloworld/controller/HelloController.java
@@ -3,17 +3,17 @@ package com.springboot.springboothelloworld.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class HelloController {
 
     private Map<String, Object> result = new HashMap<>();
 
     @RequestMapping("/hello")
-    @ResponseBody
     public Map<String, Object> hello() {
         result.put("name", "Mohtadi");
         result.put("city", "Tunisia");
