6ce9ca6cc4edcd7aef6a60075e05bf769bb63e97
nasrimohtadi@gmail.com <Awkward@009>
2020-03-18 21:49:33 +0100

    Hello Servlet, Filter and Listener

--- src/main/java/com/springboot/springboothelloworld/SpringBootHelloworldApplication.java
+++ src/main/java/com/springboot/springboothelloworld/SpringBootHelloworldApplication.java
@@ -2,8 +2,10 @@ package com.springboot.springboothelloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringBootHelloworldApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/springboot/springboothelloworld/servlet/HelloListener.java
@@ -0,0 +1,20 @@
+package com.springboot.springboothelloworld.servlet;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class HelloListener implements ServletContextListener {
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("Servlet Context Destroyed");
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("Servlet Context Initialized");
+    }
+
+}
